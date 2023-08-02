ea4bbe3df7c2c5f9169d05bc74e61f47eb1be4f6
AndrewZakhari <andrewzakhari5@gmail.com>
2023-08-02 19:20:13 +0300

    Fixed Responsiveness Bug

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Skills from '@/components/skills'
 export default function Home() {
   
   return (
-    <div className='bg-black w-screen overflow-x-hidden'>
+    <div className='bg-black  overflow-x-hidden'>
       
       <Header />
       <Skills />
